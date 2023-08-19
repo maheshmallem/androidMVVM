7de4a6707c5969bbd9dce4da0b936054faf2626b
mahesh504 <mallemmahesh@gmail.com>
2023-08-19 11:10:00 +0530

    added increment app with out mvvm

--- app/src/main/java/com/example/mvvmtest/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtest/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.mvvmtest
 import Observer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var incrementValue :Int = 0;
+    lateinit var txtContent :TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtContent = findViewById(R.id.txt_content);
 
         // Add observer for activity life cycle
         lifecycle.addObserver(Observer());
+    }
+    fun increment(view:View){
+        incrementValue ++;
+        settext();
+    }
+    fun  settext() {
+       txtContent.text = incrementValue.toString();
     }
 }
