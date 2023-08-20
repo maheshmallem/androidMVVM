06d14f6765ad00032fcb2f1f05f7e200aa702897
mahesh504 <mallemmahesh@gmail.com>
2023-08-20 09:27:36 +0530

    added binding and intial data

--- app/src/main/java/com/example/mvvmtest/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtest/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.mvvmtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainBinding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val postObj = Post("Intruduction to mvvm", "Introduction to MVVM Architecture Introduction to MVVM Architecture Model-View-ViewModel…", "https://next-studios.blogspot.com/2023/08/mvvm-architecture-in-android.html")
+        mainBinding.post = postObj;
 
     }
 
---
+++ app/src/main/java/com/example/mvvmtest/Post.kt
@@ -0,0 +1,3 @@
+package com.example.mvvmtest
+
+data class Post(val title:String, val description :String, val url :String)
